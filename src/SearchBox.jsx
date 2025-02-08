5f966fb6b8bc774cd9a86db2e43717191da6afa2
Kavya2512p <kavya2512p@gmail.com>
2025-02-08 18:19:33 +0530

    fourth commit

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -1,17 +1,54 @@
-import React from 'react'
+// import React from 'react'
+
+// export default function SearchBox(props) {
+//     return (
+//         <div >
+//             <div className='fs-1 text-center pb-2 dancing-script '>Welcome to the Temperature Checker!</div>
+//             <input type="text" placeholder='Enter city name to search' className='form-control'
+//                 onKeyDown={(e) => {
+//                     if (e.key === "Enter") {
+//                         props.recieveHandler(e.target.value);
+//                     }
+//                 }}
+//             />
+//         </div>
+//     )
+// }
+
+import React, { useState } from 'react';
+import 'font-awesome/css/font-awesome.min.css';
 
 export default function SearchBox(props) {
+    const [city, setCity] = useState('');
+
+    const handleSearch = () => {
+        if (city) {
+            props.recieveHandler(city);
+        }
+    };
+
     return (
-        <div >
-            <div className='fs-1 text-center pb-2 dancing-script '>Welcome to the Temperature Checker!</div>
-            <input type="text" placeholder='search here' className='form-control'
-                onKeyDown={(e) => {
-                    if (e.key === "Enter") {
-                        props.recieveHandler(e.target.value);
-                    }
-                }}
-            />
+        <div>
+            <div className='fs-1 text-center pb-2 dancing-script'>Welcome to the Temperature Checker!</div>
+            <div className='input-group'>
+                <input
+                    type="text"
+                    value={city}
+                    onChange={(e) => setCity(e.target.value)}
+                    placeholder='Enter city name to search'
+                    className='form-control'
+                    onKeyDown={(e) => {
+                        if (e.key === "Enter") {
+                            handleSearch();
+                        }
+                    }}
+                />
+                <span className="input-group-text" onClick={handleSearch} style={{ cursor: 'pointer' }}>
+                    <i className="fa fa-search"></i>
+                </span>
+            </div>
         </div>
-    )
+    );
 }
+
 
